b72fa29d10444e6a61212cb662a4f1c941b21af1
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-10 19:48:47 +0500

    Processing data from database

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,20 +1,33 @@
-import { listings } from "./../listings";
+import { Database, Listing } from "../lib/types";
+import { ObjectId } from "mongodb";
 
 export const resolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     },
   },
 
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
+      if (!deleteRes.value) {
+        throw new Error("Failed to delete  Listing");
       }
-      throw new Error("List not deleted");
+      return deleteRes.value;
     },
+  },
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString(),
   },
 };
