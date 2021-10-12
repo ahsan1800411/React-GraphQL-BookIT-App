d1a031ba365c9209cb865b0f09fdca60f90ad668
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-12 09:55:54 +0500

    Delete Listing

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,4 +1,9 @@
 import { server } from "../../lib/api";
+import {
+  DeleteListingData,
+  ListingsData,
+  DeleteListingVariables,
+} from "./types";
 
 interface Props {
   title: string;
@@ -19,16 +24,39 @@ const LISTINGS = `
         }
     }
 `;
+const DELETE_LISTING = `
+    mutation DeleteListing($id:ID!){
+        deleteListing(id:$id){
+            id
+            
+        }
+    }
+`;
 
 export default function Listings({ title }: Props) {
   const fetchListings = async () => {
-    const { data } = await server.fetch({ query: LISTINGS });
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     console.log(data);
   };
+
+  const deleteListing = async () => {
+    const { data } = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "6162f81b0bd14fc7cf2d7792",
+      },
+    });
+    console.log(data);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings</button>
+      <button onClick={deleteListing}>Delete a Listing</button>
     </div>
   );
 }
