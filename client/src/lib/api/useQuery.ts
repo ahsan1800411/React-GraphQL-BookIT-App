05b6a3501829d823e246bc45b743a9b7bcb4b32d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-10-12 16:01:52 +0500

    Initial Commit

--- client/src/lib/api/useQuery.ts
+++ client/src/lib/api/useQuery.ts
@@ -3,17 +3,30 @@ import { server } from "./server";
 
 interface State<TData> {
   data: TData | null;
+  loading: boolean;
+  error: boolean;
 }
 
 export const useQuery = <TData = any>(query: string) => {
   const [state, setState] = useState<State<TData>>({
     data: null,
+    loading: false,
+    error: false,
   });
 
   const fetch = useCallback(() => {
     const fetchApi = async () => {
-      const { data } = await server.fetch<TData>({ query });
-      setState({ data });
+      try {
+        setState({ loading: true, data: null, error: false });
+        const { data, errors } = await server.fetch<TData>({ query });
+        if (errors && errors.length) {
+          throw new Error(errors[0].message);
+        }
+        setState({ data, loading: false, error: false });
+      } catch (error) {
+        setState({ error: true, loading: false, data: null });
+        throw console.error(error);
+      }
     };
     fetchApi();
   }, [query]);
--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -34,7 +34,7 @@ const DELETE_LISTING = `
 `;
 
 export default function Listings({ title }: Props) {
-  const { data, refetch } = useQuery<ListingsData>(LISTINGS);
+  const { data, refetch, loading, error } = useQuery<ListingsData>(LISTINGS);
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
@@ -58,6 +58,9 @@ export default function Listings({ title }: Props) {
       ))}
     </ul>
   ) : null;
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Something went wrong...</h2>;
 
   return (
     <div>
